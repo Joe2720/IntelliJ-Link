cde4b173472ecee02945a154c8c1a2c89c30a8fc
Joe <you@example.com>
2019-10-29 19:04:15 +0100

    Grundkoden er skrevet

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Regnemaskine minRegn = new Regnemaskine();
+
+        System.out.println(minRegn.cylinder(2,2));
+    }
+}
